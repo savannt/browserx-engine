a4a6704663fe5ab24e83d3127f663bf0b7413011
Savant <savant.eclipse@gmail.com>
2022-03-31 15:33:07 -0400

    Fix

--- RemoteBrowser.js
+++ RemoteBrowser.js
@@ -23,7 +23,7 @@ module.exports = async (emulation) => {
         userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36";
     }
     const pup = require("puppeteer-extra").use(stealth);
-    const chromePaths = require("chrome-paths");
+    // const chromePaths = require("chrome-paths");
 
     const browser = await pup.launch({
         // executablePath: this.pup.executablePath().replace('app.asar', 'app.asar.unpacked'),
