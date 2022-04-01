45f9214fba9f397b2424dd35015c751d443f0e0b
Savant <savant.eclipse@gmail.com>
2022-04-01 12:40:31 -0400

    Fizx

--- client.js
+++ client.js
@@ -3,9 +3,9 @@
 const WS_URL = "ws://10.128.0.7:8060";
 
 const uuid = require("uuidv4").uuid();
+const createRemoteBrowser = require("./RemoteBrowser");
 
 const WebSocket = require("ws");
-const createRemoteBrowser = require("./RemoteBrowser");
 const ws = new WebSocket(WS_URL);
 
 console.log("Client started!");
@@ -23,8 +23,26 @@ ws.on("open", () => {
             if(message.type === "create") {
                 let uuid = message.uuid;
                 let emulation = message.emulation;
+                let cdpClient;
 
                 console.log(`Creating browser with uuid: ${uuid}, emulation: ${emulation}`);
+
+                const cdp = new WebSocket(wsURL);
+                cdp.on("open", () => {
+                    const server = new WebSocket.Server({ port: 8060 });
+                    server.on("connection", client => {
+                        cdpClient = client;
+                        client.on("message", (data) => {
+                            cdp.send(data);
+                        });
+                    });
+
+                    cdp.on("message", (data) => {
+                        cdpClient.send(data);
+                    });
+                });
+
+
                 let wsURL = await createRemoteBrowser(emulation);
                 wsURL = wsURL.replace("127.0.0.1", "35.193.47.127");
                 console.log(`Browser created on endpoint: ${wsURL}`);
@@ -33,7 +51,7 @@ ws.on("open", () => {
                 ws.send(JSON.stringify({
                     type: "created",
                     uuid,
-                    endpoint: wsURL
+                    endpoint: `https://35.193.47.127:8060`
                 }));
             }
         }
