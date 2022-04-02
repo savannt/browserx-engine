1506179d37088f1051da4fc922c7f225f6d8797d
Savant <savant.eclipse@gmail.com>
2022-04-01 23:21:34 -0400

    Fix

--- test.js
+++ test.js
@@ -36,8 +36,6 @@
 
 class CloudWS {
     constructor() {
-        const ws = require("ws");
-        this.ws = new ws("ws://34.121.23.61:8060");
         const EventEmitter = require("events");
         this.events = new EventEmitter();
         this.init = false;
@@ -46,30 +44,27 @@ class CloudWS {
     async createRemote (uuid, emulation) {
         if(!this.init) await this.listen();
         return await new Promise(r => {
-            console.log("Creating browser...");
             this.ws.send(JSON.stringify({
                 type: "create",
                 uuid,
                 emulation,
             }));
-            this.ws.once(`created:${uuid}`, d => r(d));
+            this.events.once(`created:${uuid}`, d => r(d));
         });
     }
 
     listen () {
         this.init = true;
+        const ws = require("ws");
+        this.ws = new ws("ws://34.121.23.61:8060");
         return new Promise(r => {
             this.ws.on("open", () => {
 
-                console.log("[BROWSER-MANAGER] Connected to CloudWS".bgBlue);
+                console.log("[BROWSER-MANAGER] Connected to CloudWS");
 
                 this.ws.on("message", data => {
-
-                    console.log("Received: ");
-                    console.log(data);
                     const message = JSON.parse(data);
                 
-
                     if(message && message.type) {
 
                         if(message.type === "created") {
@@ -77,9 +72,7 @@ class CloudWS {
                             const uuid = message.uuid;
                             const endpoint = message.endpoint;
 
-                            this.events.send(`created:${uuid}`, endpoint);
-                            console.log("Browser created on endpoint: " + endpoint);
-
+                            this.events.emit(`created:${uuid}`, endpoint);
                         }
                     }
                 });
@@ -98,5 +91,6 @@ class CloudWS {
     const cloud = new CloudWS();
     console.log("starting");
     await cloud.listen();
-    console.log(await cloud.createRemote("test", "none"));
+    let remote = await cloud.createRemote("test", "none");
+    console.log("remote: " + remote);
 })();
