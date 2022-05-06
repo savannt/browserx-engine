29c28a18971737e2e2ed41af771f026a41f7f61d
Savant <savant.eclipse@gmail.com>
2022-05-06 13:12:25 -0400

    Changed startup script

--- server/startupClient.js
+++ server/startupClient.js
@@ -4,8 +4,13 @@ console.log("[Startup] Starting client");
 const exec = require("child_process").exec;
 exec("npm i", (err, stdout, stderr) => {
     console.log("[Startup] Client installed dependencies");
-    gitpull(__dirname, (err, out) => {
-        console.log("[Startup] Client pulled latest code");
-        require("./client.js")(false);
+
+    exec("pm2 link qirsigsob1arlad 07hc86pxfzy4reh", (err, stdout, stderr) => {
+        console.log("[Startup] Linked to pm2.io");
+    
+        gitpull(__dirname, (err, out) => {
+            console.log("[Startup] Client pulled latest code");
+            require("./client.js")(false);
+        });
     });
 });
--- server/startupHost.js
+++ server/startupHost.js
@@ -4,8 +4,13 @@ console.log("[Startup] Starting host");
 const exec = require("child_process").exec;
 exec("npm i", (err, stdout, stderr) => {
     console.log("[Startup] Host installed dependencies");
-    gitpull(__dirname, (err, out) => {
-        console.log("[Startup] Host pulled latest code");
-        require("./host.js");
+    
+    exec("pm2 link qirsigsob1arlad 07hc86pxfzy4reh", (err, stdout, stderr) => {
+        console.log("[Startup] Linked to pm2.io");
+        
+        gitpull(__dirname, (err, out) => {
+            console.log("[Startup] Host pulled latest code");
+            require("./host.js");
+        });
     });
 });
