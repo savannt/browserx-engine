da90ad96661e147fc3be45d663a7697d884bf676
Savant <savant.eclipse@gmail.com>
2022-05-06 13:29:06 -0400

    Fixed startup scripts

--- server/startupClient.js
+++ server/startupClient.js
@@ -1,12 +1,18 @@
 const gitpull = require("git-pull");
 
 console.log("[Startup] Starting client");
-const exec = require("child_process").exec;
-exec("npm i", (err, stdout, stderr) => {
-    console.log("[Startup] Client installed dependencies");
 
-    gitpull(__dirname, (err, out) => {
-        console.log("[Startup] Client pulled latest code");
-        require("./client.js")(false);
-    });
+gitpull(__dirname, (err, out) => {
+    console.log("[Startup] Client pulled latest code");
+    require("./client.js")(false);
 });
+
+// const exec = require("child_process").exec;
+// exec("npm i", (err, stdout, stderr) => {
+//     console.log("[Startup] Client installed dependencies");
+
+//     gitpull(__dirname, (err, out) => {
+//         console.log("[Startup] Client pulled latest code");
+//         require("./client.js")(false);
+//     });
+// });
--- server/startupHost.js
+++ server/startupHost.js
@@ -1,16 +1,27 @@
 const gitpull = require("git-pull");
 
 console.log("[Startup] Starting host");
-const exec = require("child_process").exec;
-exec("npm i", (err, stdout, stderr) => {
-    console.log("[Startup] Host installed dependencies");
+
+exec(`pm2 link qirsigsob1arlad 07hc86pxfzy4reh host${Math.floor(Math.random() * 999)}`, (err, stdout, stderr) => {
+    console.log("[Startup] Linked to pm2.io");
     
-    exec("pm2 link qirsigsob1arlad 07hc86pxfzy4reh", (err, stdout, stderr) => {
-        console.log("[Startup] Linked to pm2.io");
-        
-        gitpull(__dirname, (err, out) => {
-            console.log("[Startup] Host pulled latest code");
-            require("./host.js");
-        });
+    gitpull(__dirname, (err, out) => {
+        console.log("[Startup] Host pulled latest code");
+        require("./host.js");
     });
 });
+
+
+// const exec = require("child_process").exec;
+// exec("npm i", (err, stdout, stderr) => {
+//     console.log("[Startup] Host installed dependencies");
+    
+//     exec("pm2 link qirsigsob1arlad 07hc86pxfzy4reh", (err, stdout, stderr) => {
+//         console.log("[Startup] Linked to pm2.io");
+
+//         gitpull(__dirname, (err, out) => {
+//             console.log("[Startup] Host pulled latest code");
+//             require("./host.js");
+//         });
+//     });
+// });
