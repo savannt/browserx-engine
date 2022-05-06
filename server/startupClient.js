adb3774be436125d421e7ae53bc23a54486cd5c7
Savant <savant.eclipse@gmail.com>
2022-05-06 12:44:06 -0400

    Added "npm i" on startup

--- server/startupClient.js
+++ server/startupClient.js
@@ -1,5 +1,11 @@
 const gitpull = require("git-pull");
 
-gitpull(__dirname, (err, out) => {
-    require("./client.js");
+console.log("[Startup] Starting client");
+const exec = require("child_process").exec;
+exec("npm i", (err, stdout, stderr) => {
+    console.log("[Startup] Client installed dependencies");
+    gitpull(__dirname, (err, out) => {
+        console.log("[Startup] Client pulled latest code");
+        require("./client.js")(false);
+    });
 });
--- server/startupHost.js
+++ server/startupHost.js
@@ -1,5 +1,11 @@
 const gitpull = require("git-pull");
 
-gitpull(__dirname, (err, out) => {
-    require("./host.js");
+console.log("[Startup] Starting host");
+const exec = require("child_process").exec;
+exec("npm i", (err, stdout, stderr) => {
+    console.log("[Startup] Host installed dependencies");
+    gitpull(__dirname, (err, out) => {
+        console.log("[Startup] Host pulled latest code");
+        require("./host.js")(false);
+    });
 });
