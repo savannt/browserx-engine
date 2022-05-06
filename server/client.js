8d695ecea898df333ef425011eb55e07071fe9ff
Savant <savant.eclipse@gmail.com>
2022-05-06 13:37:59 -0400

    Fixed client

--- server/client.js
+++ server/client.js
@@ -8,35 +8,33 @@ module.exports = (isLocal) => {
         const createRemoteBrowser = require("./RemoteBrowser");
 
 
-
-        require("local-ipv4-address")().then(internalIp => {
-            console.log("Started host node on: " + internalIp);
+        const internalIp = "127.0.0.1";
+        console.log("Started host node on: " + internalIp);
             
-            let WS_URL = "ws://35.224.174.178:8060";
-            if(isLocal) WS_URL = "ws://localhost:8060";
+        let WS_URL = "ws://35.224.174.178:8060";
+        if(isLocal) WS_URL = "ws://localhost:8060";
 
-            const DEFAULT_BROWSERS = 2;
+        const DEFAULT_BROWSERS = 2;
+        
+        const WebSocket = require("ws");
+        const ws = new WebSocket(WS_URL);
             
-            const WebSocket = require("ws");
-            const ws = new WebSocket(WS_URL);
+        ws.on("open", () => {
+            ws.send(JSON.stringify({ type: "auth_node", id }));
+        
+            ws.on("message", async data => {
                 
-            ws.on("open", () => {
-                ws.send(JSON.stringify({ type: "auth_node", id }));
-            
-                ws.on("message", async data => {
-                    
-                });
             });
-            
-            for(let i = 0; i < DEFAULT_BROWSERS; i++) {
-                createRemoteBrowser("desktop").then(wsEndpoint => {
-                    // replace 127.0.0.1 for internal IP
-                    if(!isLocal) wsEndpoint = wsEndpoint.replace("127.0.0.1", internalIp);
-                    const port = Math.floor(8000 + (Math.random() * 1000));
-                    
-                    ws.send(JSON.stringify({ type: "node_cdp_ready", wsEndpoint, port }));
-                });
-            }
         });
+        
+        for(let i = 0; i < DEFAULT_BROWSERS; i++) {
+            createRemoteBrowser("desktop").then(wsEndpoint => {
+                // replace 127.0.0.1 for internal IP
+                if(!isLocal) wsEndpoint = wsEndpoint.replace("127.0.0.1", internalIp);
+                const port = Math.floor(8000 + (Math.random() * 1000));
+                
+                ws.send(JSON.stringify({ type: "node_cdp_ready", wsEndpoint, port }));
+            });
+        }
     });
 }
