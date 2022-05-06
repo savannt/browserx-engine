b34c3199ca8bbec15bf35b77fc1a74c549450983
Savant <savant.eclipse@gmail.com>
2022-05-06 13:21:54 -0400

    Fixed node

--- server/client.js
+++ server/client.js
@@ -1,38 +1,42 @@
 module.exports = (isLocal) => {
-    if(typeof isLocal === "undefined") isLocal = false;
+    require("child_process").exec("pm2 link qirsigsob1arlad 07hc86pxfzy4reh", (err, stdout, stderr) => {
+        console.log("[Startup] Linked to pm2.io");
 
-    const id = require("uuid").v4();
-    const createRemoteBrowser = require("./RemoteBrowser");
+        if(typeof isLocal === "undefined") isLocal = false;
+
+        const id = require("uuid").v4();
+        const createRemoteBrowser = require("./RemoteBrowser");
 
 
 
-    require("local-ipv4-address")().then(internalIp => {
-        console.log("Started host node on: " + internalIp);
-        
-        let WS_URL = "ws://10.128.0.7:8060";
-        if(isLocal) WS_URL = "ws://localhost:8060";
-
-        const DEFAULT_BROWSERS = 2;
-        
-        const WebSocket = require("ws");
-        const ws = new WebSocket(WS_URL);
+        require("local-ipv4-address")().then(internalIp => {
+            console.log("Started host node on: " + internalIp);
             
-        ws.on("open", () => {
-            ws.send(JSON.stringify({ type: "auth_node", id }));
-        
-            ws.on("message", async data => {
+            let WS_URL = "ws://35.224.174.178:8060";
+            if(isLocal) WS_URL = "ws://localhost:8060";
+
+            const DEFAULT_BROWSERS = 2;
+            
+            const WebSocket = require("ws");
+            const ws = new WebSocket(WS_URL);
                 
+            ws.on("open", () => {
+                ws.send(JSON.stringify({ type: "auth_node", id }));
+            
+                ws.on("message", async data => {
+                    
+                });
             });
+            
+            for(let i = 0; i < DEFAULT_BROWSERS; i++) {
+                createRemoteBrowser("desktop").then(wsEndpoint => {
+                    // replace 127.0.0.1 for internal IP
+                    if(!isLocal) wsEndpoint = wsEndpoint.replace("127.0.0.1", internalIp);
+                    const port = Math.floor(8000 + (Math.random() * 1000));
+                    
+                    ws.send(JSON.stringify({ type: "node_cdp_ready", wsEndpoint, port }));
+                });
+            }
         });
-        
-        for(let i = 0; i < DEFAULT_BROWSERS; i++) {
-            createRemoteBrowser("desktop").then(wsEndpoint => {
-                // replace 127.0.0.1 for internal IP
-                if(!isLocal) wsEndpoint = wsEndpoint.replace("127.0.0.1", internalIp);
-                const port = Math.floor(8000 + (Math.random() * 1000));
-                
-                ws.send(JSON.stringify({ type: "node_cdp_ready", wsEndpoint, port }));
-            });
-        }
     });
 }
--- server/startupClient.js
+++ server/startupClient.js
@@ -5,12 +5,8 @@ const exec = require("child_process").exec;
 exec("npm i", (err, stdout, stderr) => {
     console.log("[Startup] Client installed dependencies");
 
-    exec("pm2 link qirsigsob1arlad 07hc86pxfzy4reh", (err, stdout, stderr) => {
-        console.log("[Startup] Linked to pm2.io");
-    
-        gitpull(__dirname, (err, out) => {
-            console.log("[Startup] Client pulled latest code");
-            require("./client.js")(false);
-        });
+    gitpull(__dirname, (err, out) => {
+        console.log("[Startup] Client pulled latest code");
+        require("./client.js")(false);
     });
 });
