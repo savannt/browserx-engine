75c94ebdf1b87afc85492e4603fcd5ed17fb42b0
Savant <savant.eclipse@gmail.com>
2022-05-06 13:36:14 -0400

    Fixed startup

--- server/startupHost.js
+++ server/startupHost.js
@@ -2,7 +2,7 @@ const gitpull = require("git-pull");
 
 console.log("[Startup] Starting host");
 
-exec(`pm2 link qirsigsob1arlad 07hc86pxfzy4reh host${Math.floor(Math.random() * 999)}`, (err, stdout, stderr) => {
+require("child_process").exec(`pm2 link qirsigsob1arlad 07hc86pxfzy4reh host${Math.floor(Math.random() * 999)}`, (err, stdout, stderr) => {
     console.log("[Startup] Linked to pm2.io");
     
     gitpull(__dirname, (err, out) => {
