d05fc5c6fde09899d782bbbfe6aabfc9ddd0bd74
Savant <savant.eclipse@gmail.com>
2022-05-06 12:44:24 -0400

    Fixed Startup scripts

--- server/startupHost.js
+++ server/startupHost.js
@@ -6,6 +6,6 @@ exec("npm i", (err, stdout, stderr) => {
     console.log("[Startup] Host installed dependencies");
     gitpull(__dirname, (err, out) => {
         console.log("[Startup] Host pulled latest code");
-        require("./host.js")(false);
+        require("./host.js");
     });
 });
