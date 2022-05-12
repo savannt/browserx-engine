4393f0462757ef988fd241f691cec31ac4185208
Savant <savant.eclipse@gmail.com>
2022-05-12 19:24:22 -0400

    Fix

--- server2.0/host/startup.js
+++ server2.0/host/startup.js
@@ -1,45 +1,38 @@
 const gitpull = require("git-pull");
 const fs = require("fs");
 
-// if first start
-if (!fs.existsSync("./config.json")) {
-    // git pull
+const configExists = fs.existsSync("./config.json");
+let config;
+if(configExists) config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+
+if(!configExists) {
+    config = {
+        port: 8060,
+        host: "127.0.0.1",
+        isLocal: false,
+        lastRestart: 0
+    };
+    fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
+}
+
+const port = config.port;
+const host = config.host;
+const isLocal = config.isLocal;
+console.log(Date.now());
+console.log(config.lastRestart);
+const sinceLastRestart = Date.now() - config.lastRestart;
+
+console.log("[Startup] Time since last restart: " + sinceLastRestart + "ms");
+if(sinceLastRestart > 1000 * 10 || sinceLastRestart < 0) {
+    console.log("[Startup] Not enough time since last restart, restarting...");
+    
+    config.lastRestart = Date.now();
+    fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
+
     gitpull(__dirname, (err, out) => {
-        console.log("[Startup] Pulled latest code, restarting...");
-        // create config.json
-        fs.writeFileSync("./config.json", JSON.stringify({
-            port: 8060,
-            host: "127.0.0.1",
-            isLocal: false,
-            lastRestart: Date.now()
-        }));
-        // stop process, pm2 will auto start again
         process.exit(0);
     });
 } else {
-    const config = require("./config.json");
-    const port = config.port;
-    const host = config.host;
-    const isLocal = config.isLocal;
-    console.log(Date.now());
-    console.log(config.lastRestart);
-    const sinceLastRestart = Date.now() - config.lastRestart;
-
-    console.log("[Startup] Time since last restart: " + sinceLastRestart + "ms");
-    if(sinceLastRestart > 1000 * 10 || sinceLastRestart < 0) {
-        console.log("[Startup] Not enough time since last restart, restarting...");
-
-        fs.writeFileSync("./config.json", JSON.stringify({
-            port,
-            host,
-            isLocal,
-            lastRestart: Date.now()
-        }));
-
-        process.exit(0);
-    }
-
-
     console.log("[Startup] Started host on version " + require("./about.json").version);
     require("./index.js")(port, host, isLocal);
 }
--- server2.0/node/startup.js
+++ server2.0/node/startup.js
@@ -1,45 +1,37 @@
 const gitpull = require("git-pull");
 const fs = require("fs");
 
-// if first start
-if (!fs.existsSync("./config.json")) {
-    // git pull
+const configExists = fs.existsSync("./config.json");
+let config;
+if(configExists) config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+
+if(!configExists) {
+    config = {
+        port: 8060,
+        host: "127.0.0.1",
+        isLocal: false,
+        browsers: 2,
+        lastRestart: 0
+    };
+    fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
+}
+
+const port = config.port;
+const host = config.host;
+const isLocal = config.isLocal;
+const browsers = config.browsers;
+const sinceLastRestart = Date.now() - config.lastRestart;
+
+if(sinceLastRestart > 1000 * 10 || sinceLastRestart < 0) {
+    console.log("[Startup] Not enough time since last restart, restarting...");
+    
+    config.lastRestart = Date.now();
+    fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
+
     gitpull(__dirname, (err, out) => {
-        console.log("[Startup] Pulled latest code, restarting...");
-        // create config.json
-        fs.writeFileSync("./config.json", JSON.stringify({
-            port: 8060,
-            host: "127.0.0.1",
-            isLocal: false,
-            browsers: 2,
-            lastRestart: Date.now()
-        }));
-        // stop process, pm2 will auto start again
         process.exit(0);
     });
 } else {
-    const config = require("./config.json");
-    const port = config.port;
-    const host = config.host;
-    const isLocal = config.isLocal;
-    const browsers = config.browsers;
-    const sinceLastRestart = Date.now() - config.lastRestart;
-
-    if(sinceLastRestart > 1000 * 10 || sinceLastRestart < 0) {
-        console.log("[Startup] Not enough time since last restart, restarting...");
-
-        fs.writeFileSync("./config.json", JSON.stringify({
-            port,
-            host,
-            isLocal,
-            browsers,
-            lastRestart: Date.now()
-        }));
-
-        process.exit(0);
-    }
-
-
     console.log("[Startup] Started node on version " + require("./about.json").version);
     require("./index.js")(port, host, isLocal);
 }
